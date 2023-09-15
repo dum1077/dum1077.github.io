02920ec3f9facea94c051626450e034a7208af9c
kim <kajinori@gachon.ac.kr>
2023-09-15 18:03:29 +0900

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -27,3 +27,17 @@ document.addEventListener('scroll', () => {
     arrowUp.classList.remove('arrow-up_visible')
   }}
 )
+
+
+// Navbar
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+
+navbarToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+// Navbar 메뉴 클릭시 메뉴를 자동으로 닫아줌
+navbarMenu.addEventListener('click', () => {
+  navbarMenu.classList.remove('open');
+});
