360d90f523c508192f41464b7b6caf9142f24eca
kim <kajinori@gachon.ac.kr>
2023-09-18 19:20:09 +0900

    JS-observer

---
+++ src/active_menu.js
@@ -0,0 +1,60 @@
+// 구현 계획
+// 1. 모든 섹션 요소들과 메뉴 아이템들을 가지고 온다.
+// 2. IntersectionObserver 를 사용해서 모든 섹션들을 관찰해야한다.
+// 3. 보여지는 섹션에 해당하는 메뉴아이템을 활성화 시킨다.
+// 보여지는 섹션:
+//  - 다수의 섹션이 동시에 보여진다면, 가장 첫번째 섹션을 선택
+//  - 마지막 contact 섹션이 보여진다면, 가장 마지막 섹션을 선택
+
+
+const sectionIds = ['#home', '#about', '#skills', '#work', '#testimonial', '#contact'];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[href="${ id }"]`));
+
+const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
+
+const options = {
+  rootMargin: '-20% 0px 0px 0px',
+  threshold: [0, 0.99],
+}
+
+const observer = new IntersectionObserver(observerCallback, options);
+sections.forEach(section => observer.observe(section));
+
+function observerCallback(entries) {
+  let selectLastOne; // flag 변수
+  entries.forEach(entry => {
+    const index = sectionIds.indexOf(`#${ entry.target.id }`);
+    visibleSections[index] = entry.isIntersecting;
+    selectLastOne =
+      index === sectionIds.length - 1 &&
+      entry.isIntersecting &&
+      entry.intersectionRatio >= 0.95;
+  });
+  // console.log(visibleSections)
+  // console.log('무조건 라스트 섹션', selectLastOne);
+
+  const navIndex = selectLastOne
+    ? sectionIds.length - 1
+    : findFirstIntersecting(visibleSections);
+  // console.log(sectionIds[navIndex]);
+
+  selectNavItems(navIndex)
+
+
+
+}
+
+function findFirstIntersecting(intersections) {
+  const index = intersections.indexOf(true);
+  return index >= 0 ? index : 0;
+}
+
+function selectNavItems(index) {
+  const navItem = navItems[index]; // 새롭게 선택될 item
+  if (!navItem) return; // 안전을 위해 빨리 종료 (잘못된 배열크기 입력시)
+  activeNavItem.classList.remove('active');
+  activeNavItem = navItem;
+  activeNavItem.classList.add('active');
+}
--- src/projects.js
+++ src/projects.js
@@ -1,26 +1,27 @@
 'use strict';
 
 // 프로젝트 필터링 관련 로직 처리
-
 const categories = document.querySelector('.categories');
 const projects = document.querySelectorAll('.project');
 const projectsContainer = document.querySelector('.projects');
-
-
 
 categories.addEventListener('click', (event) => {
   const filter = event.target.dataset.category;
   if (filter == null) {
     return;
   }
+  //액티브 메뉴를 재설정
   handleActiveSelection(event.target);
+
+  //프로젝트 필터링
   filterProjects(filter);
+
 });
 
 function handleActiveSelection(target) {
   const active = document.querySelector('.category--selected');
   active.classList.remove('category--selected');
-  target.target.classList.add('category--selected');
+  target.classList.add('category--selected');
 }
 
 function filterProjects(filter) {
@@ -37,81 +38,19 @@ function filterProjects(filter) {
   }, 250);
 }
 
-// categories.forEach(category => {
-//   category.addEventListener('click', () => {
-//     categories.forEach(c => c.classList.remove('category--selected'));
-//     category.classList.add('category--selected');
-//   });
-// });
-
-// front.addEventListener('click', () => {
-//   if (front.className === 'category category--selected') {
-//     front.classList.add('project__front');
-//   }
-// })
-//
-// back.addEventListener('click', () => {
-//   if (back.className === 'category category--selected') {
-//     back.classList.add('project__back');
-//   }
-// })
-// HTML 요소들을 가져옵니다.
-//const categories = document.querySelectorAll('.category');
-// const projects = document.querySelectorAll('.project');
-//
-// // 각 버튼에 대한 클릭 이벤트 리스너를 등록합니다.
-// categories.forEach(category => {
-//   category.addEventListener('click', () => {
-//     // 선택된 버튼에 'category--selected' 클래스를 추가합니다.
-//     categories.forEach(c => c.classList.remove('category--selected'));
-//     category.classList.add('category--selected');
-//
-//     // 선택된 카테고리를 확인합니다.
-//     const selectedCategory = category.getAttribute('id');
-//
-//     // 모든 프로젝트를 숨깁니다.
-//     projects.forEach(project => {
-//       project.style.display = 'none';
-//     });
-//
-//     // 선택된 카테고리에 해당하는 프로젝트만 표시합니다.
-//     if (selectedCategory === 'all') {
-//       projects.forEach(project => {
-//         project.style.display = 'block';
-//       });
-//     } else {
-//       projects.forEach(project => {
-//         if (project.classList.contains(selectedCategory)) {
-//           project.style.display = 'block';
-//         }
-//       });
+// const io = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     // 관찰 대상이 viewport 안에 들어온 경우 'tada' 클래스를 추가
+//     if (entry.intersectionRatio > 0) {
+//       entry.target.classList.add('active');
 //     }
-//   });
-// });
-//
-// categories.forEach(category => {
-//   category.addEventListener('click', () => {
-//     // ...
-//
-//     // 선택된 카테고리에 해당하는 프로젝트만 표시합니다.
-//     if (selectedCategory === 'all') {
-//       projects.forEach(project => {
-//         project.classList.add('show'); // 애니메이션 클래스 추가
-//       });
-//     } else {
-//       projects.forEach(project => {
-//         if (project.classList.contains(selectedCategory)) {
-//           project.classList.add('show'); // 애니메이션 클래스 추가
-//         } else {
-//           project.classList.remove('show'); // 다른 카테고리의 프로젝트는 숨깁니다.
-//         }
-//       });
+//     // 그 외의 경우 'tada' 클래스 제거
+//     else {
+//       entry.target.classList.remove('active');
 //     }
-//   });
-// });
+//   })
+// })
+
+// 관찰할 대상을 선언하고, 해당 속성을 관찰시킨다.
 
 
-
-// 카테고리 클릭시 클릭된 카테고리가 가지고 있는 타입에 따라
-// 프로젝트마다 고유한 정보들을 가지고 있어서
-// 그것에 맞게 필터링 하기.
